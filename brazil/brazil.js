37d3008615ee46e1d413e50dd81b4922fcf1e441
jhvonsky <jhvonsky@gmail.com>
2021-06-25 20:50:34 +0700

    Add all files

---
+++ brazil/brazil.js
@@ -0,0 +1,52 @@
+const ctx = document.getElementById('brazilChart').getContext('2d')
+
+function myChart(){ 
+    fetch(`https://covid19.mathdro.id/api/countries/brazil`)
+        .then(res => res.json())
+        .then(res => {
+            let result = res
+            let arr = []
+
+            for (const i in result) {
+                arr.push(result)
+            }
+
+            let recover = arr[0].recovered.value
+            let deaths = arr[0].deaths.value
+            let confirmed = arr[0].confirmed.value
+            
+
+            new Chart(ctx, {
+                type: 'polarArea',
+                data: {
+                    labels: ['Confirm', 'Death', 'Recovered'],
+                    datasets: [{
+                        data: [
+                            confirmed,
+                            deaths,
+                            recover
+                        ],
+                        backgroundColor: [
+                            'red',
+                            'black',
+                            'aqua'
+                        ]
+                    }]
+                },
+
+                options: {
+                    title: {
+                        display: true,
+                        text: 'Covid 19 in Brazil',
+                        fontSize: 25,
+                        fontFamily:'Arial'
+                    },
+                    legend: {
+                        position:'right'
+                    }
+                }
+            })
+        })
+}
+
+export { myChart }
---
+++ main/app.js
@@ -0,0 +1,4 @@
+import { myChart as brazil } from '../brazil/brazil.js'
+import { myChart as usa } from '../usa/usa.js'
+brazil()
+usa()
